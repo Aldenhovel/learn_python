145deda9442ae46da9eb0d689c44841beb09fd56
Aldenhovel Liang <39938763+Aldenhovel@users.noreply.github.com>
2021-04-21 22:21:50 +0800

    Update and rename web_spider 网络爬虫/grab.py to web_spider 网络爬虫/src/example_1/grab_phone_info.py

--- web_spider 网络爬虫/src/example_1/grab_phone_info.py
+++ web_spider 网络爬虫/src/example_1/grab_phone_info.py
@@ -4,6 +4,9 @@ import xml.etree.ElementTree as ET
 import re
 
 def reSearch(rule, text):
+    '''
+    封装的正则筛选方法
+    '''
     try:
         block = re.search(rule, text).span()
         res = text[block[0]:block[1]]
@@ -12,6 +15,9 @@ def reSearch(rule, text):
         return None
 
 def PrettyXml(element, indent='\t', newline='\n', level=0):
+    '''
+    将xml文件整理成符合阅读的格式
+    '''
     if element:
         if (element.text is None) or element.text.isspace():
             element.text = newline + indent * (level + 1)
@@ -27,9 +33,19 @@ def PrettyXml(element, indent='\t', newline='\n', level=0):
 
 
 def _getPageText(url):
+    '''
+    获取对应URL的网页源代码
+    '''
     return requests.get(url).text
 
 def _getCellPhoneData(text):
+    '''
+    从_getPageText()返回的网页源代码中整理出各个手机的信息，包括：
+        手机名：name
+        详细信息URL地址：URL
+        图片URL地址：picURL
+    以列表形式返回
+    '''
     soup = BeautifulSoup(text, features='html.parser')
     body = soup.body
     dt = body.find('div', class_='wrapper clearfix')\
@@ -48,8 +64,41 @@ def _getCellPhoneData(text):
             pass
     return res_dt
 
+def _saveToXML(dt):
+    '''
+    将_getCellPhoneData()的返回列表存储到"./phones.xml上"
+    '''
+    path = "phones.xml"
+    tree = ET.parse(path)
+    root = tree.getroot()
+    for data in dt:
+        phone = ET.Element("phone")
+        name = ET.Element("name")
+        name.text = data['name']
+        URL = ET.Element("URL")
+        URL.text = data['URL']
+        picURL = ET.Element('picURL')
+        picURL.text = data['picURL']
+        phone.append(name)
+        phone.append(URL)
+        phone.append(picURL)
+        root.append(phone)
+    PrettyXml(root)
+    tree.write(path, encoding='utf-8', xml_declaration=True)
+    print('finish...')
+    
 def _getScore():
-    path = "name.xml"
+    '''
+    根据"./phones.xml"记录的手机型号爬取对应的评分，包括：
+        总体评价：total_score
+        性价比：v4m
+        性能：power
+        续航：battery
+        外观：look
+        拍照：snap
+    存储到"./phones.xml"中
+    '''
+    path = "phones.xml"
     tree = ET.parse(path)
     root = tree.getroot()
     phones = root.findall('phone')
@@ -73,7 +122,6 @@ def _getScore():
             for score_block in score_block_list:
                 score = score_block.find('div', class_='circle-value').text
                 dt.append(score)
-
             total_score = ET.Element('total_score')
             total_score.text = dt[0]
             value_for_money = ET.Element('v4m')
@@ -86,14 +134,12 @@ def _getScore():
             look.text = dt[4]
             snap = ET.Element('snap')
             snap.text = dt[5]
-
             phone.append(total_score)
             phone.append(value_for_money)
             phone.append(power)
             phone.append(battery)
             phone.append(look)
             phone.append(snap)
-
             print(dt)
             PrettyXml(root)
             tree.write(path, encoding='utf-8', xml_declaration=True)
@@ -103,29 +149,7 @@ def _getScore():
             print('已跳过一条......')
 
 
-
-def _saveToXML(dt):
-    path = "name.xml"
-    tree = ET.parse(path)
-    root = tree.getroot()
-    for data in dt:
-        phone = ET.Element("phone")
-        name = ET.Element("name")
-        name.text = data['name']
-        URL = ET.Element("URL")
-        URL.text = data['URL']
-        picURL = ET.Element('picURL')
-        picURL.text = data['picURL']
-        phone.append(name)
-        phone.append(URL)
-        phone.append(picURL)
-        root.append(phone)
-    PrettyXml(root)
-    tree.write(path, encoding='utf-8', xml_declaration=True)
-    print('finish...')
-
 if __name__ == "__main__":
-
     for i in range(1, 15):
         url = 'https://detail.zol.com.cn/cell_phone_index/subcate57_0_list_1_0_1_2_0_$.html'.replace('$', str(i))
         text = _getPageText(url)
